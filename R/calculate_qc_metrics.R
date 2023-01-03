c05b566182e30c4fd4b609263a6be49f86ea28af
Victor Yuan <victor.2wy@gmail.com>
2023-01-03 13:23:09 -0800

    remove return_qc parameter

--- R/calculate_qc_metrics.R
+++ R/calculate_qc_metrics.R
@@ -1,27 +1,26 @@
 #' calculate_qc_metrics
 #'
 #' @param scobj a seurat object
-#' @param return_qc logical whether to return a tibble with QC metrics
 #'
 #' @return a tibble
 #' @export
 #'
 #' @examples
-calculate_qc_metrics <- function(scobj, return_qc = FALSE) {
+calculate_qc_metrics <- function(scobj) {
   metadata <- scobj@meta.data %>% tibble::as_tibble(rownames = "cellid")
 
-  if (return_qc) {
-    metadata <- metadata %>%
+  # extract QC metrics
+  metadata <- metadata %>%
 
-      # rename columns
-      dplyr::rename(n_umi = nCount_RNA,
-                    n_gene = nFeature_RNA) %>%
+    # rename columns
+    dplyr::rename(n_umi = nCount_RNA,
+                  n_gene = nFeature_RNA) %>%
 
-      # calculate log10genesperumi and mtRatio
-      dplyr::mutate(
-        qc_log10genesperumi = log10(nGene) / log10(nUMI),
-        qc_mtfraction = Seurat::PercentageFeatureSet(scobj, pattern = "^MT-")$nCount_RNA / 100,
-        qc_rbfraction = Seurat::PercentageFeatureSet(scobj, pattern = "^RB-")$nCount_RNA / 100)
-  }
+    # calculate log10genesperumi and mtRatio
+    dplyr::mutate(
+      qc_log10genesperumi = log10(nGene) / log10(nUMI),
+      qc_mtfraction = Seurat::PercentageFeatureSet(scobj, pattern = "^MT-")$nCount_RNA / 100,
+      qc_rbfraction = Seurat::PercentageFeatureSet(scobj, pattern = "^RB-")$nCount_RNA / 100)
+
   metadata
 }
