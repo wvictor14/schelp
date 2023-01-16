101d48caf1ece308e6c9b0af32252570f8ccbd76
Victor Yuan <victor.2wy@gmail.com>
2023-01-16 12:18:11 -0800

    plot_genebycell uses facet_grid instead of facet_wrap, pexpr moved to plot instead of axis label

---
+++ R/plot_genebycell.R
@@ -0,0 +1,150 @@
+#' plots expression by cell cluster
+#'
+#' Use extract_gene so that gene expression exists as columns of metadata.
+#'
+#' @param metadata cell metadata
+#' @param genes character vector of gene names
+#' @param expr count matrix, cells in columns, genes in rows
+#'
+#' @param cellid_col column in metadata containing cell identifiers, should
+#' match rownames(counts). Default is "cellid"
+#' @param celltype_col column in metadata referring to cell type identity
+#'
+#' @param type one of "mean_se", "bar_mean", "bar_mean_se", "bar_median",
+#' "violin", "boxplot"
+#' @param zscore Whether expression should be zscore or not
+#' @param add_jitter adds jittered points default = FALSE,
+#' @param jitterheight multiplier that controls jitter height. Default = 0.75
+#' @param alpha controls the alpha of jittered points = 0.5
+#' @param pointsize = 1,
+#' @param fontsize_p Multiplier that controls relative fontsize. Default is 1
+#' @param pexpr_label_size controls size of percent expression labels. Default
+#' is 2.5
+#'
+#' @return ggplot2
+#' @export
+#' @import ggplot2
+plot_genebycell <- function(metadata, cellid_col = 'cellid', expr, genes,
+                              celltype_col,
+                              type = 'boxplot',
+                              zscore = FALSE,
+                              add_jitter = FALSE,
+                              jitterheight = 0.75,
+                              alpha = 0.5,
+                              pointsize = 1,
+                              ncol = NULL,
+                              nrow = NULL,
+                              fontsize_p = 1){
+  label <- 'ln(TP10K)'
+
+  # extract gene expr
+  metadata <- extract_gene(
+    metadata = metadata, expr = expr, cellid_col = cellid_col, genes = genes)
+
+  # reshape to tidy format
+  metadata <- metadata %>%
+    dplyr::select(cellid, {{celltype_col}},
+                  tidyselect::any_of(genes)) %>%
+    tidyr::pivot_longer(cols = tidyselect::any_of(genes),
+                        names_to = 'gene',
+                        values_to = 'expression')
+
+
+  # calculate % expression
+  metadata <- metadata %>%
+    dplyr::group_by( gene, {{celltype_col}}) %>%
+    dplyr::summarize(p_expr = sum(expression > 0) / dplyr::n()) %>%
+    dplyr::mutate(
+      p_expr_highlight = ifelse(p_expr > 0, 'black', 'grey'),
+      p_expr = p_expr %>% scales::percent(accuracy = 0.1) %>%
+                    stringr::str_pad(width = 5)) %>%
+    dplyr::ungroup() %>%
+
+    dplyr::arrange({{celltype_col}}) %>%
+    dplyr::mutate(# refactor
+                  celltype_pexpr = paste0(
+                    {{celltype_col}} %>% stringr::str_pad(width = 2),
+                    glue::glue("({p_expr})") %>% stringr::str_pad(width = 8)
+                  ) %>%
+                    forcats::fct_inorder()
+    ) %>%
+    dplyr::left_join(metadata,
+                     .,
+                     by = c('gene', rlang::as_name(ensym(celltype_col))))
+
+  # calculate z score
+  if (zscore) {
+    metadata <- metadata %>%
+      dplyr::group_by(gene)  %>%
+      dplyr::mutate(expression_zscore = (expression - mean(expression)) / sd(expression))
+
+    label <- glue::glue('Z score {label}')
+  }
+
+  # start plot
+  plot <- ggplot(metadata, aes(y = {{celltype_col}}, x = expression, color = p_expr_highlight)) +
+    facet_grid(cols = vars(gene), scales = 'free') +
+    geom_text(aes(label = p_expr),
+              x = -0.25, size = 2.5, family = 'mono') +
+    scale_color_identity()
+
+
+  # select geom and summary function
+  if (type == 'mean_se') {
+    plot <- plot +
+      stat_summary(fun.data = mean_se)
+  }
+
+  if (type == 'bar_mean') {
+    plot <- plot +
+      stat_summary(fun = mean, geom = 'bar')
+    label <- glue::glue('mean {label}')
+  }
+
+  if (type == 'bar_mean_se') {
+    plot <- plot +
+      stat_summary(fun = mean, geom = 'bar') +
+      stat_summary(fun.data = mean_se, geom = "errorbar")
+    label <- glue::glue('mean {label}')
+  }
+
+  if (type == 'bar_median') {
+    plot <- plot +
+      stat_summary(fun = median, geom = 'bar')
+    label <- glue::glue('median {label}')
+  }
+
+  if (type == 'violin') {
+    plot <- plot +
+      geom_violin()
+  }
+  if (type == 'boxplot') {
+    plot <- plot +
+      geom_boxplot(outlier.shape = ifelse(add_jitter, NA, 19))
+  }
+
+  if (add_jitter) {
+
+    if (type == 'violin') {
+      plot <- plot +
+        ggforce::geom_sina(alpha = alpha, size = pointsize, adjust = 1*jitterheight)
+    } else {
+      plot <- plot +
+        geom_jitter(alpha = alpha, size = pointsize, height = 0.2*jitterheight)
+    }
+  }
+
+  plot <- plot +
+    # add theme customization
+    scale_x_continuous(expand = expansion(add = 0.5)) +
+    theme_bw(base_size = 10*fontsize_p) +
+    theme(panel.grid.major.y = element_blank(),
+          panel.grid.minor.y = element_blank(),
+          panel.spacing = unit(0, 'lines'),
+          axis.line = element_line(),
+          axis.ticks = element_blank(),
+          strip.background = element_blank(),
+          strip.placement = 'outside') +
+    labs(x = label)
+  return(plot)
+}
---
+++ tests/testthat/test-plot_genebycell.R
@@ -0,0 +1,30 @@
+test_that("plot_genebycell works with 1 gene", {
+  counts <- scibd::counts
+  metadata <- scibd::metadata
+
+  plot_genebycell(metadata = metadata, genes = 'FOXP3', expr = counts,
+                    celltype_col = ori.Cluster_group)
+  plot_genebycell(metadata = metadata, genes = 'NOX1', expr = counts,
+                    celltype_col = ori.Cluster_group)
+  plot_genebycell(metadata = metadata, genes = 'MS4A10', expr = counts,
+                    celltype_col = ori.Cluster_group)
+  expect_true(TRUE)
+})
+
+test_that("plot_genebycell works with 2 genes", {
+  counts <- scibd::counts
+  metadata <- scibd::metadata
+
+  genes <- c('FOXP3', 'NOX1')
+  plot_genebycell(metadata = metadata, genes = genes, expr = counts,
+                    celltype_col = ori.Cluster_group)
+
+  genes <- c('MS4A10', 'NXPE1')
+  plot_genebycell(metadata = metadata, genes = genes, expr = counts,
+                    celltype_col = ori.Cluster_group)
+
+  expect_true(TRUE)
+})
+test_that("multiplication works", {
+  expect_equal(2 * 2, 4)
+})
