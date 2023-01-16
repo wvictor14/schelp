3ba60f5b66e0182bafaf0805257b1c541518322c
Victor Yuan <victor.2wy@gmail.com>
2023-01-16 11:14:50 -0800

    update test for plot_bar_cellgene

--- tests/testthat/test-plot_bar_cellgene.R
+++ tests/testthat/test-plot_bar_cellgene.R
@@ -2,18 +2,12 @@ test_that("plot_bar_cellgene works with 1 gene", {
   counts <- scibd::counts
   metadata <- scibd::metadata
 
-  genes <- c('FOXP3')
-  metadata$FOXP3 <- counts[genes,]
-  plot_bar_cellgene(metadata = metadata, genes = genes, celltype = ori.Cluster_group)
-
-  genes <- c('NOX1')
-  metadata$NOX1 <- counts[genes,]
-  plot_bar_cellgene(metadata = metadata, genes = genes, celltype = ori.Cluster_group)
-
-  genes <- c('MS4A10')
-  metadata$MS4A10 <- counts[genes,]
-  plot_bar_cellgene(metadata = metadata, genes = genes, celltype = ori.Cluster_group)
-
+  plot_bar_cellgene(metadata = metadata, genes = 'FOXP3', expr = counts,
+                    celltype = ori.Cluster_group)
+  plot_bar_cellgene(metadata = metadata, genes = 'NOX1', expr = counts,
+                    celltype = ori.Cluster_group)
+  plot_bar_cellgene(metadata = metadata, genes = 'MS4A10', expr = counts,
+                    celltype = ori.Cluster_group)
   expect_true(TRUE)
 })
 
@@ -22,11 +16,12 @@ test_that("plot_bar_cellgene works with 2 genes", {
   metadata <- scibd::metadata
 
   genes <- c('FOXP3', 'NOX1')
-  metadata <- dplyr::bind_cols(metadata, t(as.matrix(counts[genes,])))
-  plot_bar_cellgene(metadata = metadata, genes = genes, celltype = ori.Cluster_group)
+  plot_bar_cellgene(metadata = metadata, genes = genes, expr = counts,
+                    celltype = ori.Cluster_group)
 
   genes <- c('MS4A10', 'NXPE1')
-  metadata <- dplyr::bind_cols(metadata, t(as.matrix(counts[genes,])))
-  plot_bar_cellgene(metadata = metadata, genes = genes, celltype = ori.Cluster_group)
+  plot_bar_cellgene(metadata = metadata, genes = genes, expr = counts,
+                    celltype = ori.Cluster_group)
+
   expect_true(TRUE)
 })
