1668c96d4a96fb854eae713fe5103e215df91468
Victor Yuan <victor.2wy@gmail.com>
2023-01-16 11:13:55 -0800

    add cellid_col argument to extract_gene, update documentation

--- R/extract_gene.R
+++ R/extract_gene.R
@@ -1,13 +1,20 @@
 #' extracts gene expression data into metadata
 #'
+#' First checks if gene names are in counts rownames
+#' If genes already exist in metadata, message that they are being overwritten
+#' rownames of counts will be joined on a cell identifier column in metadata
+#' using left_join
+#'
 #' @param metadata cell metadata
+#' @param cellid_col column in metadata containing cell identifiers, should
+#' match rownames(counts). Default is "cellid"
 #' @param genes character vector of gene names
 #' @param expr count matrix, cells in columns, genes in rows
 #'
 #' @return tibble
 #' @export
 #'
-extract_gene <- function(metadata, expr, genes) {
+extract_gene <- function(metadata, cellid_col = 'cellid', expr, genes) {
   # check if genes are already present in metadata
   if (any(genes %in% colnames(metadata))) {
     message('Genes exist in metadata columns')
@@ -17,6 +24,7 @@ extract_gene <- function(metadata, expr, genes) {
                                     ", ")
     ))
 
+    # remove existing gene columns
     metadata <- metadata %>%
       dplyr::select(-tidyselect::any_of(genes))
   }
@@ -29,24 +37,22 @@ extract_gene <- function(metadata, expr, genes) {
   gene_no_match <- genes[!genes %in% gene_matches]
   if (length(gene_no_match) > 0) {
     print(stringr::str_c("These genes were not found: ",
-                         stringr::str_c(gene_no_match, collapse = ", ")
-    )
-    )
+                         stringr::str_c(gene_no_match, collapse = ", ")))
   }
 
+  # if no matches found, stop
   stopifnot(length(gene_matches) > 0)
 
   extracted_data <- expr[gene_matches,,drop = FALSE] %>%
     as.matrix() %>% t() %>% tibble::as_tibble(rownames = 'cellid')
 
+  # warn if not all cells in counts are found in metadata
   if (!all(extracted_data$cellid %in% metadata$cellid)) {
-
     cells_missing <- length(setdiff(extracted_data$cellid, metadata$cellid))
-
     warning(glue::glue("{cells_missing} cells in expr data are not found in metadata$cellid"))
   }
 
-
+  # add to metadata
   metadata <- metadata %>%
     dplyr::left_join(extracted_data, by = 'cellid')
 
