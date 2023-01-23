752a899f99e5b26e0cbc1cd1d714b052a50636a5
Victor Yuan <victor.2wy@gmail.com>
2023-01-22 16:38:26 -0800

    fix errors: call library(scibd) in extract_gene tests

--- tests/testthat/test-extract_gene.R
+++ tests/testthat/test-extract_gene.R
@@ -1,18 +1,31 @@
 test_that("extract_gene works on 1 gene", {
+  library(scibd)
+  dim(metadata)
+  dim(counts)
   metadata <- extract_gene(
-    scibd::metadata, expr = scibd::counts, genes = 'FOXP3')
+    metadata = metadata, expr = counts, genes = 'FOXP3')
   expect_vector(metadata$FOXP3)
 })
 
 test_that("extract_gene works on 5 genes", {
+  library(scibd)
+  metadata <- scibd::metadata
+  counts <- scibd::counts
+
   metadata <- extract_gene(
-    scibd::metadata, expr = scibd::counts,
+    metadata = metadata, expr = counts,
     genes = c('FOXP3', 'NOX1', 'NXPE1', 'MS4A10', 'CCDC62', 'FKBP6'))
   expect_vector(metadata$FOXP3)
 })
 
 test_that("behaviour when no genes are not in counts", {
-  expect_equal(2 * 2, 4)
+  library(scibd)
+  metadata <- scibd::metadata
+  counts <- scibd::counts
+
+  expect_error(extract_gene(
+    metadata = metadata, expr = counts,
+    genes = 'fakegene'))
 })
 
 test_that("behaviour when not all genes are in counts (e.g. 1/3)", {
