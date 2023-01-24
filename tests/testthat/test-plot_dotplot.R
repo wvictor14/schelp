038832c7d87acb40664b420bad5ff8c5b9306a5c
Victor Yuan <victor.2wy@gmail.com>
2023-01-24 09:04:04 -0800

    test max zscore

--- tests/testthat/test-plot_dotplot.R
+++ tests/testthat/test-plot_dotplot.R
@@ -1,6 +1,4 @@
 test_that("plot_dotplot works with character vector gene input", {
-  counts <- scibd::counts
-  metadata <- scibd::metadata
   plot_dotplot(metadata, counts, 'FOXP3', celltype_col = ori.Cluster_group)
   plot_dotplot(metadata, counts, rownames(counts)[1:20],
                celltype_col = ori.Cluster_group)
@@ -8,8 +6,6 @@ test_that("plot_dotplot works with character vector gene input", {
 })
 
 test_that("plot_dotplot z score works", {
-  counts <- scibd::counts
-  metadata <- scibd::metadata
   plot_dotplot(metadata, counts, 'FOXP3', celltype_col = ori.Cluster_group)
   plot_dotplot(metadata, counts, rownames(counts)[10:20],
                zscore = FALSE,
@@ -18,8 +14,6 @@ test_that("plot_dotplot z score works", {
 })
 
 test_that("plot_dotplot facet_row works", {
-  counts <- scibd::counts
-  metadata <- scibd::metadata
   plot_dotplot(metadata, counts, rownames(counts)[10:20],
                zscore = FALSE,
                celltype_col = ori.Cluster_group,
@@ -28,8 +22,6 @@ test_that("plot_dotplot facet_row works", {
 })
 
 test_that("plot_dotplot works with gene list input", {
-  counts <- scibd::counts
-  metadata <- scibd::metadata
   genelist <- list(
     'Targets' = c('NOX1', 'NXPE1', 'MS4A10'),
     'Gene example' = c('FOXP3', 'FKBP6')
@@ -41,14 +33,25 @@ test_that("plot_dotplot works with gene list input", {
 })
 
 test_that("plot_dotplot works with gene list input + facet_row", {
-  counts <- scibd::counts
-  metadata <- scibd::metadata
   genelist <- list(
     'Targets' = c('NOX1', 'NXPE1', 'MS4A10'),
     'Gene example' = c('FOXP3', 'FKBP6')
   )
   plot_dotplot(metadata, counts, genes = genelist,
                zscore = FALSE,
+               celltype_col = ori.Cluster_group,
+               facet_row = Compartment)
+  expect_true(TRUE)
+})
+
+test_that("plot_dotplot max zscore", {
+  genelist <- list(
+    'Targets' = c('NOX1', 'NXPE1', 'MS4A10'),
+    'Gene example' = c('FOXP3', 'FKBP6')
+  )
+  plot_dotplot(metadata, counts, genes = genelist,
+               zscore = TRUE,
+               zscore_max = 3,
                celltype_col = ori.Cluster_group,
                facet_row = Compartment)
   expect_true(TRUE)
