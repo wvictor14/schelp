6103459141cebc5ca57a1be18dcad0a27bdecd63
Victor Yuan <victor.2wy@gmail.com>
2023-01-24 08:52:14 -0800

    merge fix conflicts

--- tests/testthat/test-extract_gene.R
+++ tests/testthat/test-extract_gene.R
@@ -1,29 +1,41 @@
 test_that("extract_gene works on 1 gene", {
   metadata <- extract_gene(
-    scibd::metadata, expr = scibd::counts, genes = 'FOXP3')
+    metadata = metadata, expr = counts, genes = 'FOXP3')
   expect_vector(metadata$FOXP3)
 })
 
 test_that("extract_gene works on 5 genes", {
   metadata <- extract_gene(
-    scibd::metadata, expr = scibd::counts,
+    metadata = metadata, expr = counts,
     genes = c('FOXP3', 'NOX1', 'NXPE1', 'MS4A10', 'CCDC62', 'FKBP6'))
   expect_vector(metadata$FOXP3)
 })
 
 test_that("behaviour when no genes are not in counts", {
-  expect_equal(2 * 2, 4)
+  expect_error(
+    extract_gene(
+      metadata = metadata, expr = counts, genes = 'NOTAGENE')
+  )
 })
 
 test_that("behaviour when not all genes are in counts (e.g. 1/3)", {
-  expect_equal(2 * 2, 4)
+  expect_warning(
+    extract_gene(
+      metadata = metadata, expr = counts, genes = c('FOXP3', 'NOTAGENE'))
+  )
 })
 
 test_that("behaviour when cells in counts are not in metadata", {
-  expect_equal(2 * 2, 4)
+  expect_warning(
+    extract_gene(
+      metadata = metadata, expr = counts[,1:6], genes = 'FOXP3')
+  )
 })
 
 test_that("behaviour when cells in metadata are not in counts", {
-  expect_equal(2 * 2, 4)
+  expect_warning(
+    extract_gene(
+      metadata = head(metadata), expr = counts, genes = 'FOXP3')
+  )
 })
 
